d82a2f4e5acbcaaca1a2f1c2c17bd404e5a4825d
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-10-07 00:51:43 +0500

    Update solver.rb
    - Added method to check a FizzBuzz String

--- solver.rb
+++ solver.rb
@@ -4,7 +4,18 @@ class Solver
 
     num * factorial(num - 1)
   end
+  
   def reverse_string(rstring)
     return rstring.reverse
+  end
+
+  def fizzbuzz(num)
+    if num%3==0 && num%5==0
+      return "FizzBuzz"
+    elsif num%3==0
+      return "Fizz"
+    elsif num%5==0
+      return "Buzz"
+    end
   end
 end
